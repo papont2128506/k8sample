e0423e64b4c947fc44dcaf59c7bf28e272d2d718
Andrey Trusov <trusov@haulmont.com>
2022-01-03 18:29:12 +0400

    Added spring-admin project

---
+++ spring-admin/src/main/java/ru/papont/springadmin/SpringAdminApplication.java
@@ -0,0 +1,13 @@
+package ru.papont.springadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAdminApplication.class, args);
+	}
+
+}
---
+++ spring-admin/src/test/java/ru/papont/springadmin/SpringAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.papont.springadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
