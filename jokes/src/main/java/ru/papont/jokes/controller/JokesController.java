5f2bd76f2e58db6be10ac98f94546d16da148e33
Andrey Trusov <trusov@haulmont.com>
2022-01-03 18:28:00 +0400

    Added jokes project

---
+++ jokes/src/main/java/ru/papont/jokes/controller/JokesController.java
@@ -0,0 +1,22 @@
+package ru.papont.jokes.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.papont.jokes.client.ChuckNorrisJokesClient;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/jokes")
+@RequiredArgsConstructor
+public class JokesController {
+
+    private final ChuckNorrisJokesClient client;
+
+    @GetMapping("random")
+    public Map<String, String> getRandomJoke() {
+        return Map.of("joke", client.getRandomJoke());
+    }
+}
